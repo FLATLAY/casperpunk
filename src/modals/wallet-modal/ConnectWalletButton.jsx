ec25abbb5c3e6e2ae4ab256c28c94caafadc04c1
bedi.mns@gmail.com <bedi.mns@gmail.com>
2023-03-05 17:56:13 +0330

    fix wallet modal

--- src/modals/wallet-modal/ConnectWalletButton.jsx
+++ src/modals/wallet-modal/ConnectWalletButton.jsx
@@ -1,87 +1,87 @@
-// import SignClient from "@walletconnect/sign-client";
-// import { Web3Modal } from "@web3modal/standalone";
+import SignClient from "@walletconnect/sign-client";
+import { Web3Modal } from "@web3modal/standalone";
 
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
-// import { WalletComponent } from "./WalletModal-style";
-// /**
-//  *
-//  * @returns {Promise<{'publicKey' : string|null , 'signature' : string|null}>}
-//  */
+import { WalletComponent } from "./WalletModal-style";
+/**
+ *
+ * @returns {Promise<{'publicKey' : string|null , 'signature' : string|null}>}
+ */
 
-// export async function connectWalletLogin() {
-//   const DEFAULT_MAIN_CHAINS = [
-//     "eip155:1",
-//     // "eip155:10",
-//     // "eip155:100",
-//     // "eip155:137",
-//     // "eip155:42161",
-//     // "eip155:42220",
-//   ];
-//   const web3Modal = new Web3Modal({
-//     projectId: "9ab28bd9133b1678f9cb98e48abc3cc7",
-//     standaloneChains: DEFAULT_MAIN_CHAINS,
-//   });
-//   const proposalNamespace = {
-//     eip155: {
-//       chains: DEFAULT_MAIN_CHAINS,
-//       methods: [
-//         "eth_sendTransaction",
-//         "eth_signTransaction",
-//         "eth_sign",
-//         "personal_sign",
-//         "eth_signTypedData",
-//       ],
-//       events: ["connect", "disconnect"],
-//     },
-//   };
-//   let account = null;
-//   const signClient = await SignClient.init({
-//     projectId: "9ab28bd9133b1678f9cb98e48abc3cc7",
-//   });
-//   const { uri, approval } = await signClient.connect({
-//     requiredNamespaces: proposalNamespace,
-//   });
-//   if (uri) {
-//     await web3Modal.openModal({
-//       route: "ConnectWallet",
-//       uri: uri,
-//       standaloneChains: DEFAULT_MAIN_CHAINS,
-//     });
-//     const session = await approval();
-//     account = session.namespaces.eip155.accounts[0].slice(9);
+export async function connectWalletLogin() {
+  const DEFAULT_MAIN_CHAINS = [
+    "eip155:1",
+    // "eip155:10",
+    // "eip155:100",
+    // "eip155:137",
+    // "eip155:42161",
+    // "eip155:42220",
+  ];
+  const web3Modal = new Web3Modal({
+    projectId: "9ab28bd9133b1678f9cb98e48abc3cc7",
+    standaloneChains: DEFAULT_MAIN_CHAINS,
+  });
+  const proposalNamespace = {
+    eip155: {
+      chains: DEFAULT_MAIN_CHAINS,
+      methods: [
+        "eth_sendTransaction",
+        "eth_signTransaction",
+        "eth_sign",
+        "personal_sign",
+        "eth_signTypedData",
+      ],
+      events: ["connect", "disconnect"],
+    },
+  };
+  let account = null;
+  const signClient = await SignClient.init({
+    projectId: "9ab28bd9133b1678f9cb98e48abc3cc7",
+  });
+  const { uri, approval } = await signClient.connect({
+    requiredNamespaces: proposalNamespace,
+  });
+  if (uri) {
+    await web3Modal.openModal({
+      route: "ConnectWallet",
+      uri: uri,
+      standaloneChains: DEFAULT_MAIN_CHAINS,
+    });
+    const session = await approval();
+    account = session.namespaces.eip155.accounts[0].slice(9);
 
-//     web3Modal.closeModal();
-//     const result = await signClient.request({
-//       request: {
-//         method: "personal_sign",
-//         params: ["Please sign this message to prove you own this account"],
-//       },
-//       chainId: "eip155:1",
-//       topic: session.topic,
-//     });
+    web3Modal.closeModal();
+    const result = await signClient.request({
+      request: {
+        method: "personal_sign",
+        params: ["Please sign this message to prove you own this account"],
+      },
+      chainId: "eip155:1",
+      topic: session.topic,
+    });
   
-//     return { account: account, signature: result };
-//   } else {
-//     return null;
-//   }
-// }
+    return { account: account, signature: result };
+  } else {
+    return null;
+  }
+}
 
-// const ConnectWalletButton = () => {
-//   const [user, setUser] = useState(null);
-//   // 3. Initialize sign client
+const ConnectWalletButton = () => {
+  const [user, setUser] = useState(null);
+  // 3. Initialize sign client
 
-//   const handleConnectWallet = async () => {
-//     let result = await connectWalletLogin();
-//     if (result) setUser(result);
-//   };
+  const handleConnectWallet = async () => {
+    let result = await connectWalletLogin();
+    if (result) setUser(result);
+  };
 
-//   return user == null ? (
-//     <WalletComponent onClick={handleConnectWallet}>
-//       Connect Wallet
-//     </WalletComponent>
-//   ) : (
-//     <WalletComponent>Initializing...' </WalletComponent>
-//   );
-// };
-// export default ConnectWalletButton;
+  return user == null ? (
+    <WalletComponent onClick={handleConnectWallet}>
+      Connect Wallet
+    </WalletComponent>
+  ) : (
+    <WalletComponent>Initializing...' </WalletComponent>
+  );
+};
+export default ConnectWalletButton;
--- src/modals/wallet-modal/casper-utils.js
+++ src/modals/wallet-modal/casper-utils.js
@@ -1,114 +1,114 @@
-// import { CLPublicKey } from "casper-js-sdk";
-// export const auth_err = {
-//   cancelled_signing: Symbol("User Cancelled Signing"),
-//   casper_signer_not_found: Symbol("Casper Signer not found"),
-//   general_error: Symbol("general error"),
-// };
-// /**
-//  *
-//  * @param {string} publicKey
-//  * @returns ({'publicKey' : string , 'account_hash' : string , 'signature' : string})
-//  */
-// let get_account_information = async function (publicKey) {
-//   let sign = await window.casperlabsHelper.signMessage(
-//     "Please sign this message to let droplinked access to your PublicKey and validate your identity.",
-//     await window.casperlabsHelper.getActivePublicKey()
-//   );
-//   return {
-//     publicKey: publicKey,
-//     account_hash: CLPublicKey.fromHex(publicKey).toAccountRawHashStr(),
-//     signature: sign,
-//   };
-// };
+import { CLPublicKey } from "casper-js-sdk";
+export const auth_err = {
+  cancelled_signing: Symbol("User Cancelled Signing"),
+  casper_signer_not_found: Symbol("Casper Signer not found"),
+  general_error: Symbol("general error"),
+};
+/**
+ *
+ * @param {string} publicKey
+ * @returns ({'publicKey' : string , 'account_hash' : string , 'signature' : string})
+ */
+let get_account_information = async function (publicKey) {
+  let sign = await window.casperlabsHelper.signMessage(
+    "Please sign this message to let droplinked access to your PublicKey and validate your identity.",
+    await window.casperlabsHelper.getActivePublicKey()
+  );
+  return {
+    publicKey: publicKey,
+    account_hash: CLPublicKey.fromHex(publicKey).toAccountRawHashStr(),
+    signature: sign,
+  };
+};
 
-// /**
-//  *
-//  * @param {({'publicKey' : string , 'account_hash' : string , 'signature' : string})=>{}} on_connected
-//  * @param {*} on_activeKeyChanged
-//  * @param {(error_type:auth_err)=>{}} on_err
-//  * @throws "Casper-Signer not found!" if the casper signer is not installed on user's browser
-//  */
-// export const casper_login = function (
-//   on_connected = null,
-//   on_err = null,
-//   on_activeKeyChanged = null
-// ) {
-//   let called = false;
-//   if (window.casperlabsHelper == undefined) {
-//     if (on_err != null && !called) {
-//       called = true;
-//       on_err(auth_err.casper_signer_not_found);
-//     }
-//     return;
-//   }
-//   window.casperlabsHelper
-//     .isConnected()
-//     .then((value) => {
-//       if (value) {
-//         window.casperlabsHelper
-//           .getActivePublicKey()
-//           .then((publicKey) => {
-//             get_account_information(publicKey)
-//               .then((acc_info) => {
-//                 if (!called) {
-//                   called = true;
-//                   on_connected(acc_info);
-//                 }
-//               })
-//               .catch((reason) => {
-//                 console.log("err 1: ", reason);
-//                 if (on_err != null && !called) {
-//                   called = true;
-//                   on_err(auth_err.cancelled_signing);
-//                 }
-//               });
-//           })
-//           .catch((reason) => {
-//             console.log("error 2: ", reason);
-//           });
-//       }
-//     })
-//     .catch((reason) => {
-//       console.log("error 3: ", reason);
-//     });
-//   window.casperlabsHelper.requestConnection();
-//   window.addEventListener("signer:connected", (msg) => {
-//     if (on_connected != null) {
-//       get_account_information(String(msg.detail.activeKey))
-//         .then((value) => {
-//           if (!called) {
-//             called = true;
-//             on_connected(value);
-//           }
-//         })
-//         .catch((error) => {
-//           console.log("error 4: ", error);
-//         });
-//     }
-//   });
-//   window.addEventListener("signer:activeKeyChanged", (msg) => {
-//     if (on_activeKeyChanged != null) {
-//       get_account_information(String(msg.detail.activeKey))
-//         .then((value) => {
-//           on_activeKeyChanged(value);
-//         })
-//         .catch((error) => {
-//           console.log("error 5: ", error);
-//         });
-//     }
-//   });
-//   window.addEventListener("signer:unlocked", (msg) => {
-//     if (on_connected != null) {
-//       get_account_information(String(msg.detail.activeKey))
-//         .then((value) => {
-//           if (!called) {
-//             called = true;
-//             on_connected(value);
-//           }
-//         })
-//         .catch((error) => {
-//           console.log("error 6:", error);
-//         });
-//     }
-//   });
-// };
+/**
+ *
+ * @param {({'publicKey' : string , 'account_hash' : string , 'signature' : string})=>{}} on_connected
+ * @param {*} on_activeKeyChanged
+ * @param {(error_type:auth_err)=>{}} on_err
+ * @throws "Casper-Signer not found!" if the casper signer is not installed on user's browser
+ */
+export const casper_login = function (
+  on_connected = null,
+  on_err = null,
+  on_activeKeyChanged = null
+) {
+  let called = false;
+  if (window.casperlabsHelper == undefined) {
+    if (on_err != null && !called) {
+      called = true;
+      on_err(auth_err.casper_signer_not_found);
+    }
+    return;
+  }
+  window.casperlabsHelper
+    .isConnected()
+    .then((value) => {
+      if (value) {
+        window.casperlabsHelper
+          .getActivePublicKey()
+          .then((publicKey) => {
+            get_account_information(publicKey)
+              .then((acc_info) => {
+                if (!called) {
+                  called = true;
+                  on_connected(acc_info);
+                }
+              })
+              .catch((reason) => {
+                console.log("err 1: ", reason);
+                if (on_err != null && !called) {
+                  called = true;
+                  on_err(auth_err.cancelled_signing);
+                }
+              });
+          })
+          .catch((reason) => {
+            console.log("error 2: ", reason);
+          });
+      }
+    })
+    .catch((reason) => {
+      console.log("error 3: ", reason);
+    });
+  window.casperlabsHelper.requestConnection();
+  window.addEventListener("signer:connected", (msg) => {
+    if (on_connected != null) {
+      get_account_information(String(msg.detail.activeKey))
+        .then((value) => {
+          if (!called) {
+            called = true;
+            on_connected(value);
+          }
+        })
+        .catch((error) => {
+          console.log("error 4: ", error);
+        });
+    }
+  });
+  window.addEventListener("signer:activeKeyChanged", (msg) => {
+    if (on_activeKeyChanged != null) {
+      get_account_information(String(msg.detail.activeKey))
+        .then((value) => {
+          on_activeKeyChanged(value);
+        })
+        .catch((error) => {
+          console.log("error 5: ", error);
+        });
+    }
+  });
+  window.addEventListener("signer:unlocked", (msg) => {
+    if (on_connected != null) {
+      get_account_information(String(msg.detail.activeKey))
+        .then((value) => {
+          if (!called) {
+            called = true;
+            on_connected(value);
+          }
+        })
+        .catch((error) => {
+          console.log("error 6:", error);
+        });
+    }
+  });
+};
