0d5f85ea74e2dc0dfbd13cd3a0050124c01d67d8
bedi.mns@gmail.com <bedi.mns@gmail.com>
2023-03-05 17:53:22 +0330

    fix profile

--- src/layouts/header/components/profile/Profile.tsx
+++ src/layouts/header/components/profile/Profile.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
+import { useSelector } from "react-redux";
 
 import { WalletIconWrapper, WalletIconComponent } from "./Profile-style";
+import { selectCurrentUser } from "../../../../store/user/user.selector";
 
 import ProfileDropdown from "./ProfileDropdown";
 import WalletModal from "../../../../modals/wallet-modal/WalletModal";
@@ -8,7 +10,7 @@ import profileIcon from "../../../../assets/icons/profile-icon.svg";
 
 const Profile = () => {
   const [showModal, setShowModal] = useState(false);
-  // const user = useSelector(selectCurrentUser);
+   const user = useSelector(selectCurrentUser);
 
   const toggleModal = () => setShowModal((p) => !p);
 
@@ -21,11 +23,11 @@ const Profile = () => {
         <WalletIconComponent src={profileIcon} />
       </WalletIconWrapper>
       <WalletModal show={showModal} close={toggleModal} />
-      {/* {user ? (
-      <WalletDropdown show={showModal} close={toggleModal} />
+      {user ? (
+      <ProfileDropdown show={showModal} close={toggleModal} />
     ) : (
       <WalletModal show={showModal} close={toggleModal} />
-    )} */}
+    )}
     </>
   );
 };
--- src/layouts/header/components/profile/ProfileDropdown.tsx
+++ src/layouts/header/components/profile/ProfileDropdown.tsx
@@ -1,6 +1,6 @@
 
 import { Box } from "@chakra-ui/react";
-//import { useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import { useAuth } from "@micro-stacks/react";
 import { useNavigate } from "react-router-dom";
 import { useMemo } from "react";
@@ -10,8 +10,8 @@ import {
   WalletDropdownComponent,
   DropdownButton,
 } from "./Profile-style";
-//import { setLogoutUser } from "../../../../../../../../store/user/user.action";
-//import { useProfile } from "../../../../../../../../hooks/useProfile/useProfile";
+import { setLogoutUser } from "../../../../store/user/user.action";
+import { useProfile } from "../../../../hooks/useProfile/useProfile";
 
 interface Props {
   close: () => void;
@@ -20,21 +20,21 @@ interface Props {
 
 
  const ProfileDropdown = ({ close, show }: Props) => {
-  //  const dispatch = useDispatch();
+    const dispatch = useDispatch();
     const navigate = useNavigate();
     const { signOut } = useAuth();
-   // const { stackAddress, walletAddress } = useProfile();
+    const { stackAddress, walletAddress } = useProfile();
   
-    // const walletAddressText = useMemo(() => {
-    //   let result = "";
-    //   if (walletAddress != "") {
-    //     result =
-    //       walletAddress.slice(0, 4) +
-    //       "...." +
-    //       walletAddress.slice(walletAddress.length - 4, walletAddress.length);
-    //   }
-    //   return result;
-    // }, []);
+    const walletAddressText = useMemo(() => {
+      let result = "";
+      if (walletAddress != "") {
+        result =
+          walletAddress.slice(0, 4) +
+          "...." +
+          walletAddress.slice(walletAddress.length - 4, walletAddress.length);
+      }
+      return result;
+    }, []);
   
     const handleChildClick = (event: React.MouseEvent<HTMLInputElement>) =>
       event.stopPropagation();
@@ -42,10 +42,10 @@ interface Props {
     if (!show) return null;
   
     const logout = () => {
-    //   dispatch(setLogoutUser());
-    //   signOut();
-    //   close();
-    //   window.location.reload();
+      dispatch(setLogoutUser());
+      signOut();
+      close();
+      window.location.reload();
     };
   
     const navigateToSetting = () => navigate("settings");
@@ -53,7 +53,7 @@ interface Props {
     return (
       <WalletDropdownBgLayout onClick={close}>
         <WalletDropdownComponent onClick={handleChildClick}>
-          {/* <DropdownButton>{walletAddressText}</DropdownButton> */}
+          <DropdownButton>{walletAddressText}</DropdownButton>
           <Box mb="8px"></Box>
           <DropdownButton>Purchase History</DropdownButton>
           <Box mb="8px"></Box>
