8dcd0f5ab1eda39f30576f02a353bb8a23468a44
bedi.mns@gmail.com <bedi.mns@gmail.com>
2023-03-06 20:10:28 +0330

    Text in banner

--- src/layouts/Event/Event.tsx
+++ src/layouts/Event/Event.tsx
@@ -1,9 +1,10 @@
 import { useMemo, useState, useEffect } from "react";
-import { Flex } from "@chakra-ui/react";
+import { Flex, Text } from "@chakra-ui/react";
 
 // const currentTime = new Date(2023, 2, 8, 21, 30, 0);
 import bannerDesktop from "../../assets/images/banner-desktop-image.svg";
 import bannerMobile from "../../assets/images/banner-mobile-image.svg";
+import textImage from "./text-icon.svg";
 
 const Event = () => {
   const [diffrence, setDiffrence] = useState(
@@ -33,10 +34,22 @@ const Event = () => {
       w="100%"
       bgImage={{ base: bannerMobile, md: bannerDesktop }}
       h={{ base: "100px", md: "80px" }}
-      backgroundSize='cover'
+      backgroundSize="cover"
       px={{ base: "21px", xl: "160px" }}
       py={{ base: "8px", xl: "0px" }}
-    ></Flex>
+    >
+      <Text
+        fontFamily="Helvetica"
+        fontSize="48px"
+        fontWeight="500"
+        color="#F9F9F6"
+        css={{
+          "-webkit-text-stroke": "2px black",
+        }}
+      >
+        Limited Drop Alert!
+      </Text>
+    </Flex>
   );
 };
 
