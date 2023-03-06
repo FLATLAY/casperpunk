f203c28808e655b8364cf8814b698043a2206e09
bedi.mns@gmail.com <bedi.mns@gmail.com>
2023-03-06 16:59:44 +0330

    implement event fucntion

--- src/layouts/Event/Event.tsx
+++ src/layouts/Event/Event.tsx
@@ -1,7 +1,35 @@
+import { useMemo, useState, useEffect } from "react";
+
+
+// const currentTime = new Date(2023, 2, 8, 21, 30, 0);
 
 
 const Event = () => {
-    return <></>
-}
 
-export default Event
+  const [diffrence, setDiffrence] = useState(
+    +new Date(2023, 2, 8, 21, 30, 0) - +new Date()
+  );
+
+
+  const getDay = () => Math.floor(diffrence / (1000 * 60 * 60 * 24));
+  const getHourse = () => Math.floor((diffrence / (1000 * 60 * 60)) % 24)
+  const getMinutes = () => Math.floor((diffrence / 1000 / 60) % 60);
+  const getSeconds = () => Math.floor((diffrence / 1000) % 60);
+
+  const days = useMemo(getDay,[diffrence])
+  const hours = useMemo(getHourse,[diffrence])
+  const minutes = useMemo(getMinutes,[diffrence])
+  const seconds = useMemo(getSeconds,[diffrence])
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+        setDiffrence( +new Date(2023, 2, 8, 21, 30, 0) - +new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [diffrence]);
+
+  return <></>;
+};
+
+export default Event;
