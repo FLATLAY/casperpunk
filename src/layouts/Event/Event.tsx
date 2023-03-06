bff889993256b2b371df6734900287ff79571f62
bedi.mns@gmail.com <bedi.mns@gmail.com>
2023-03-06 19:26:35 +0330

    add background to banner

--- src/layouts/Event/Event.tsx
+++ src/layouts/Event/Event.tsx
@@ -1,35 +1,43 @@
 import { useMemo, useState, useEffect } from "react";
-
+import { Flex } from "@chakra-ui/react";
 
 // const currentTime = new Date(2023, 2, 8, 21, 30, 0);
-
+import bannerDesktop from "../../assets/images/banner-desktop-image.svg";
+import bannerMobile from "../../assets/images/banner-mobile-image.svg";
 
 const Event = () => {
-
   const [diffrence, setDiffrence] = useState(
     +new Date(2023, 2, 8, 21, 30, 0) - +new Date()
   );
 
-
   const getDay = () => Math.floor(diffrence / (1000 * 60 * 60 * 24));
-  const getHourse = () => Math.floor((diffrence / (1000 * 60 * 60)) % 24)
+  const getHourse = () => Math.floor((diffrence / (1000 * 60 * 60)) % 24);
   const getMinutes = () => Math.floor((diffrence / 1000 / 60) % 60);
   const getSeconds = () => Math.floor((diffrence / 1000) % 60);
 
-  const days = useMemo(getDay,[diffrence])
-  const hours = useMemo(getHourse,[diffrence])
-  const minutes = useMemo(getMinutes,[diffrence])
-  const seconds = useMemo(getSeconds,[diffrence])
+  const days = useMemo(getDay, [diffrence]);
+  const hours = useMemo(getHourse, [diffrence]);
+  const minutes = useMemo(getMinutes, [diffrence]);
+  const seconds = useMemo(getSeconds, [diffrence]);
 
   useEffect(() => {
     const interval = setInterval(() => {
-        setDiffrence( +new Date(2023, 2, 8, 21, 30, 0) - +new Date());
+      setDiffrence(+new Date(2023, 2, 8, 21, 30, 0) - +new Date());
     }, 1000);
 
     return () => clearInterval(interval);
   }, [diffrence]);
 
-  return <></>;
+  return (
+    <Flex
+      w="100%"
+      bgImage={{ base: bannerMobile, md: bannerDesktop }}
+      h={{ base: "100px", md: "80px" }}
+      backgroundSize='cover'
+      px={{ base: "21px", xl: "160px" }}
+      py={{ base: "8px", xl: "0px" }}
+    ></Flex>
+  );
 };
 
 export default Event;
