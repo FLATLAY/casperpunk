298b4dc421ba70ea56ab30c285289aacc53c54e7
bedi.mns@gmail.com <bedi.mns@gmail.com>
2023-03-07 13:23:57 +0330

    add animation to banner

--- src/layouts/Event/Event.tsx
+++ src/layouts/Event/Event.tsx
@@ -1,5 +1,6 @@
 import { useMemo, useState, useEffect } from "react";
-import { Flex, Text, Image } from "@chakra-ui/react";
+import { Flex, Text, Image, keyframes, Box } from "@chakra-ui/react";
+import { motion } from "framer-motion";
 
 // const currentTime = new Date(2023, 2, 8, 21, 30, 0);
 import bannerDesktop from "../../assets/images/banner-desktop-image.svg";
@@ -7,11 +8,19 @@ import bannerMobile from "../../assets/images/banner-mobile-image.svg";
 import dotIcon from "../../assets/icons/dot-dot.svg";
 import multipliedIcon from "../../assets/icons/multiplied-icon.svg";
 
+const animationKeyframes = keyframes`
+  0% { margin-top:0px; }
+  100% { margin-top:-80px; }
+`;
+
+const animation = `${animationKeyframes} 1s ease-in-out`;
+
 const Event = () => {
   const [diffrence, setDiffrence] = useState(
     +new Date(2023, 2, 8, 21, 30, 0) - +new Date()
   );
-  //const [showBanner , setShowBanner]=useState(true)
+  const [showBanner, setShowBanner] = useState(true);
+  const [activeAnimation, setActiveAnimation] = useState(false);
 
   const getDay = () => Math.floor(diffrence / (1000 * 60 * 60 * 24));
   const getHourse = () => Math.floor((diffrence / (1000 * 60 * 60)) % 24);
@@ -31,19 +40,29 @@ const Event = () => {
     return () => clearInterval(interval);
   }, [diffrence]);
 
+  const closeEvent = () => {
+    setActiveAnimation(true);
+    setTimeout(() => {
+      setShowBanner(false);
+    }, 1000);
+  };
 
   return (
     <Flex
       w="100%"
+      as={motion.div}
+      animation={activeAnimation ? animation : ""}
       bgImage={{ base: bannerMobile, xl: bannerDesktop }}
-      flexDir={{base:'column' , xl:'row'}}
+      display={showBanner === false ? "none" : "flex"}
+      flexDir={{ base: "column", xl: "row" }}
       h={{ base: "100px", xl: "80px" }}
       backgroundSize="cover"
       px={{ base: "21px", xl: "160px" }}
       py={{ base: "8px", xl: "0px" }}
-      alignItems={{base:'start',xl:"center"}}
+      alignItems={{ base: "start", xl: "center" }}
       justifyContent="space-between"
       position="relative"
+      transition="top 2s"
     >
       <Text
         fontFamily="Helvetica"
@@ -57,7 +76,7 @@ const Event = () => {
         Limited Drop Alert!
       </Text>
 
-      <Flex gap={{base:'4px',xl:"16px"}}>
+      <Flex gap={{ base: "4px", xl: "16px" }}>
         <Flex gap="4px">
           <DigitComponent digit={Math.floor(days / 10)} />{" "}
           <DigitComponent digit={Math.floor(days % 10)} />
@@ -82,19 +101,18 @@ const Event = () => {
 
       <Image
         src={multipliedIcon}
-        pos='absolute'
+        pos="absolute"
         right="40px"
         w="34px"
         h="34px"
         borderRadius="50%"
         border="2px solid #121314"
         bg="#fff"
-        top='auto'
-        bottom='auto'
-        cursor='pointer'
+        top="auto"
+        bottom="auto"
+        cursor="pointer"
         p="6px"
-       // onClick={()=>{setShowBanner(false)}}
-
+        onClick={closeEvent}
       />
     </Flex>
   );
@@ -103,15 +121,19 @@ const Event = () => {
 const DigitComponent = ({ digit }: { digit: number }) => {
   return (
     <Flex
-      w={{base:'24px',xl:"38px"}}
-      h={{base:'32px',xl:"48px"}}
+      w={{ base: "24px", xl: "38px" }}
+      h={{ base: "32px", xl: "48px" }}
       bg="#FFFFFF"
       border="2px solid #121314"
       borderRadius="8px"
       justifyContent="center"
       alignItems="center"
     >
-      <Text fontSize={{base:'16px',xl:"24px"}} fontWeight="700" color="#121314">
+      <Text
+        fontSize={{ base: "16px", xl: "24px" }}
+        fontWeight="700"
+        color="#121314"
+      >
         {digit}
       </Text>
     </Flex>
