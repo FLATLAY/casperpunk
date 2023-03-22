61d365b66523ab21944f775af84d158a8b6ba5c6
bedi.mns@gmail.com <bedi.mns@gmail.com>
2023-03-23 01:18:33 +0430

    new banner

--- src/pages/landing/components/banner/Banner.tsx
+++ src/pages/landing/components/banner/Banner.tsx
@@ -1,10 +1,8 @@
-import { Box, Button, Image } from "@chakra-ui/react";
-
+import { Box, Button, Image, Text } from "@chakra-ui/react";
 
 import bannerImage from "../../../../assets/images/banner-image.svg";
 
 const Banner = () => {
-  
   const clickOnButton = () => {
     const element = document.getElementById("product-section");
     if (element) {
@@ -13,41 +11,46 @@ const Banner = () => {
   };
 
   return (
-    <Box w="100%" h="auto" maxH="470px" position="relative">
+    <Box w="100%" h="auto" position="relative">
       <Image
         src={bannerImage}
         w="100%"
         h="auto"
         display={{ base: "none", md: "block" }}
+        maxH="100%"
       />
-      {/* <Box
-        backgroundImage={mobileBanner}
-        w="100%"
-        h="470px"
-        backgroundSize="cover"
-        backgroundRepeat="no-repeat"
-        display={{ base: "block", md: "none" }}
-      /> */}
+      <Text
+        fontFamily="Montserrat"
+        fontWeight="800"
+        fontSize="56px"
+        color="#FFFFFF"
+        position="absolute"
+        top="15%"
+        left="15%"
+      >
+        Explore The
+        <br /> CasperPunks <br /> Collection
+      </Text>
       <Button
         position="absolute"
-        bg="#C00E1A"
+        bg="#161F2F"
         w="256px"
         borderRadius="4px"
         padding="16px"
-        left={{ base: "50%", md: "11.04%" }}
+        left={{ base: "50%", md: "15%" }}
         mr={{ base: "50%", md: "0" }}
         transform={{ base: "translate(-50%, -50%)", md: "translate(0%, 0%)" }}
-        top={{ base: "400px", md: "55%" }}
+        top={{ base: "400px", md: "65%" }}
         fontWeight="800"
         fontSize="16px"
         color="white"
         h="56px"
         _hover={{
-          bg: "#C00E1A",
+          bg: "#333",
         }}
         onClick={clickOnButton}
       >
-        SHOP COLLECTION NOW
+        GET YOUR TEE
       </Button>
       {/* <Image
         src={mobileBanner}
