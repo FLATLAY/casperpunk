03bf58a81895adf778f9688e34002243b9926027
bedi.mns@gmail.com <bedi.mns@gmail.com>
2023-03-23 19:23:55 +0430

    dropdown position

--- src/layouts/header/components/profile/Profile.tsx
+++ src/layouts/header/components/profile/Profile.tsx
@@ -16,7 +16,7 @@ const Profile = () => {
   return (
     <>
       <WalletIconWrapper
-        bg={showModal ? "#fff" : "transparent"}
+        //bg={showModal ? "#fff" : "transparent"}
         onClick={toggleModal}
       >
         <WalletIconComponent src={profileIcon} />
