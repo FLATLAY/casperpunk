682cfdeef50de8e436fe1810395c944edfc14128
bedi.mns@gmail.com <bedi.mns@gmail.com>
2023-03-24 16:48:03 +0430

    fix

--- src/pages/address/components/AddressPageButtons.tsx
+++ src/pages/address/components/AddressPageButtons.tsx
@@ -1,16 +1,16 @@
 import { Flex, Box } from "@chakra-ui/react";
 import { useNavigate, useParams } from "react-router-dom";
-import { useSelector } from "react-redux";
+// import { useSelector } from "react-redux";
 import { useState } from "react";
 
 import BasicButton, {
   BUTTON_TYPE,
 } from "../../../components/basic-button/BasicButton";
-import { useCart } from "../../../hooks/useCart/useCart";
-import { getShopifyData } from "./address-utils";
+// import { useCart } from "../../../hooks/useCart/useCart";
+// import { getShopifyData } from "./address-utils";
 import { useApi } from "../../../hooks/useApi/useApi";
-import { postCreatCheckout } from "../../../apis/shopifyApi";
-import { selectCurrentUser } from "../../../store/user/user.selector";
+// import { postCreatCheckout } from "../../../apis/shopifyApi";
+// import { selectCurrentUser } from "../../../store/user/user.selector";
 import { useToastify } from "../../../context/ToastifyContext/ToastifyContext";
 import { postAddCheckoutAddress } from "../../../apis/checkoutsApi";
 
@@ -19,40 +19,44 @@ const AddressPageButtons = ({ selectedAddress }: { selectedAddress: any }) => {
   const [loading, setLoading] = useState(false);
 // hooks
   const navigate = useNavigate();
-  const { shopName } = useParams();
+ // const { shopName } = useParams();
   const { postApi } = useApi();
-  const { cartItems, isShopifyCart } = useCart();
+  //const { cartItems, isShopifyCart } = useCart();
   const { errorToast } = useToastify();
-  const profile = useSelector(selectCurrentUser);
+ // const profile = useSelector(selectCurrentUser);
 
   const navigateToShipping = async () => {
     if (!selectedAddress) {
       errorToast("Select address");
       return;
     }
-    if (isShopifyCart()) {
-      const shopifyData = getShopifyData(
-        selectedAddress,
-        cartItems,
-        profile.email
-      );
-      setLoading(true);
-      let result = await postApi(
-        postCreatCheckout(shopifyData, cartItems[0].product.shopifyShopDomain)
-      );
-      setLoading(false);
-      if (result) {
-        window.open(result.checkout.web_url);
-      }
-    } else {
-      setLoading(true);
-      let result = await postApi(postAddCheckoutAddress(selectedAddress._id))
-      setLoading(false);
-      if(result) navigate(`/${shopName}/shipping`);
-    }
+    // if (isShopifyCart()) {
+    //   const shopifyData = getShopifyData(
+    //     selectedAddress,
+    //     cartItems,
+    //     profile.email
+    //   );
+    //   setLoading(true);
+    //   let result = await postApi(
+    //     postCreatCheckout(shopifyData, cartItems[0].product.shopifyShopDomain)
+    //   );
+    //   setLoading(false);
+    //   if (result) {
+    //     window.open(result.checkout.web_url);
+    //   }
+    // } else {
+    //   setLoading(true);
+    //   let result = await postApi(postAddCheckoutAddress(selectedAddress._id))
+    //   setLoading(false);
+    //   if(result) navigate(`/shipping`);
+    // }
+    setLoading(true);
+    let result = await postApi(postAddCheckoutAddress(selectedAddress._id))
+    setLoading(false);
+    if(result) navigate(`/shipping`);
   };
 
-  const navigateToCart = () => navigate(`/${shopName}/cart`);
+  const navigateToCart = () => navigate(`/cart`);
 
   return (
     <Flex
