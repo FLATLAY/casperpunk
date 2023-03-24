eaf6ad3b98eadb185373f8eb96b3c0271a1f698c
bedi.mns@gmail.com <bedi.mns@gmail.com>
2023-03-24 21:03:24 +0430

    change return url

--- src/pages/checkout/components/buttons-component/CheckoutForm.tsx
+++ src/pages/checkout/components/buttons-component/CheckoutForm.tsx
@@ -25,7 +25,7 @@ const CheckoutForm = () => {
       //`Elements` instance that was used to create the Payment Element
       elements,
       confirmParams: {
-        return_url: "https://example.com/order/123/complete",
+        return_url: "https://casper.droplinked.com/",
       },
     });
 
