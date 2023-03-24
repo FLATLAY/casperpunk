5af64e906b50dc52e35dd5fefdbbf522de905c0d
bedi.mns@gmail.com <bedi.mns@gmail.com>
2023-03-24 17:02:49 +0430

    get shippings functions

--- src/pages/shipping/Shipping.tsx
+++ src/pages/shipping/Shipping.tsx
@@ -1,8 +1,36 @@
 import { Flex } from "@chakra-ui/react";
+import { useState, useEffect } from "react";
+
+import { useApi } from "../../hooks/useApi/useApi";
+import { getCartShippingRates } from "../../apis/checkoutsApi";
 
 import PageContainer from "../../components/page-container/PageContainer";
+import Loading, { LOADING_SIZE } from "../../components/loading/Loading";
 
 const Shipping = () => {
+  const [shippings, setShippings] = useState(null);
+  // const [loading, setLoading] = useState(false);
+
+  const { getApi } = useApi();
+
+  const getShippings = async () => {
+    const result = await getApi(getCartShippingRates());
+    if (result) setShippings(result.shippingPrice);
+  };
+
+  useEffect(() => {
+    getShippings();
+  }, []);
+
+  if (!shippings)
+    return (
+      <PageContainer>
+        <Flex justifyContent="center" alignItems="center" w="100%">
+          <Loading size={LOADING_SIZE.LARGE} />
+        </Flex>
+      </PageContainer>
+    );
+
   return (
     <PageContainer>
       <Flex
