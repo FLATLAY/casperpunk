5373146bea14681bed8f57e3e516196c5ba2e4bf
bedi.mns@gmail.com <bedi.mns@gmail.com>
2023-03-25 17:00:24 +0430

    add payment via casper function

---
+++ src/pages/checkout/components/buttons-component/CasperButton.tsx
@@ -0,0 +1,22 @@
+import { Box, Button } from "@chakra-ui/react";
+
+const CasperButton = () => {
+  return (
+    <Button
+      //  display="flex"
+      //  justifyContent="center"
+      //  alignItems="center"
+      padding="16px 60px"
+      gap="8px"
+      width="77px"
+      height="55px"
+      background="#27262B"
+      borderRadius="4px"
+      color="white"
+    >
+      Pay Casper
+    </Button>
+  );
+};
+
+export default CasperButton;
---
+++ src/pages/checkout/components/buttons-component/casper-utils.js
@@ -0,0 +1,57 @@
+import { CLPublicKey, CLU512, DeployUtil } from "casper-js-sdk";
+import * as casper_consts from './constants'
+
+/**
+ * 
+ * @param {DeployUtil.Deploy} deploy_object 
+ */
+async function transfer_details(deploy_object) {    
+    if (!deploy_object.isTransfer()){
+        return false;
+    }
+    let amount = Number(deploy_object.session.asTransfer().args.args.get("amount").data)/(1000000000);
+    let target = CLPublicKey.fromEd25519(deploy_object.session.asTransfer().args.args.get("target").data);
+    let sender = deploy_object.header.account.toHex();
+    return{
+        'sender' : sender,
+        'amount_in_cspr' : amount,
+        'target' : target.toHex()
+    }
+}
+/**
+ * 
+ * @param {number} usd_amount is the amount of USDs you want to convert to motes (each mote is 10^-9 CSPR) 
+ * @returns  the amount of motes that are equal to $usd_amount dollars 
+ */
+async function getCasperRatio(usd_amount){
+    let js = await (await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=casper-network&order=market_cap_desc&per_page=4&page=1&sparkline=false")).json();
+    return new CLU512(Math.round(usd_amount * (1000000000/js[0].current_price)));   
+}
+async function customerPayment1(sender_publicKey, reciver_publicKey, amount_in_usd){
+    let amount_of_motes = (await getCasperRatio(amount_in_usd));
+    const toPublicKey = CLPublicKey.fromHex(reciver_publicKey);
+    const fromPublicKey = CLPublicKey.fromHex(sender_publicKey);
+    const deploy = DeployUtil.makeDeploy(
+        new DeployUtil.DeployParams(fromPublicKey, casper_consts.network , 1, 1800000),
+        DeployUtil.ExecutableDeployItem.newTransfer(amount_of_motes.value(), toPublicKey, null, 0),
+        DeployUtil.standardPayment(100000000)
+    );
+    const json = DeployUtil.deployToJson(deploy);
+    const signature = await window.casperlabsHelper.sign(json, sender_publicKey,reciver_publicKey).catch((reason)=>{
+        console.log("cancelled by ueser");
+        return "cancelled";
+    });
+    const deployObject = DeployUtil.deployFromJson(signature);
+    const signed = await casper_consts.casperClient.putDeploy(deployObject.val);
+    let transfer_detail = await transfer_details(deployObject.val);
+    return {
+        'deploy_hash' : signed,
+        'sender' : transfer_detail.sender,
+        'target' : transfer_detail.target,
+        'cspr_amount' : transfer_detail.amount_in_cspr
+    }
+}
+export { customerPayment1 } 
+
+// usage : 
+//console.log(await customerPayment1("0144f5adf499591351807bc83490314262bd6846beee80a16269a83c9901ecec8a", "01eb9b0e8e73de521f86f40666d985529ae316aff5ace6c4049a42364f442e0e76", 1));
