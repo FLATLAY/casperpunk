91c01449aeaeb4e431ea65dd75c15602cf98631d
bedi.mns@gmail.com <bedi.mns@gmail.com>
2023-03-26 05:15:42 +0430

    nav to main page

--- src/layouts/navbar/Navbar.jsx
+++ src/layouts/navbar/Navbar.jsx
@@ -1,30 +1,53 @@
 import { Flex, Image, Text } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 
 import casperLogo from "../../assets/images/casper-logo.svg";
 import burgerIcon from "../../assets/icons/burger-icon.svg";
 //
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
+  const clickOnLogo = () => navigate("/");
   return (
     <Flex
       w="100%"
-      p={{base:'20px',md:"30px 24px 30px 40px" ,xl:"30px 24px 30px 100px"}}
+      p={{
+        base: "20px",
+        md: "30px 24px 30px 40px",
+        xl: "30px 24px 30px 100px",
+      }}
       alignItems="center"
       justifyContent="space-between"
     >
-      <Image src={casperLogo} w={{base:'80px',md:"126px" }}h={{base:'24px',md:"39px" }} />
+      <Image
+        src={casperLogo}
+        onClick={clickOnLogo}
+        cursor='pointer'
+        w={{ base: "80px", md: "126px" }}
+        h={{ base: "24px", md: "39px" }}
+      />
 
-      <Flex w="auto" gap={{base:'12px' , xl:'24px'}} display={{base:'none' , md:'flex'}} >
+      <Flex
+        w="auto"
+        gap={{ base: "12px", xl: "24px" }}
+        display={{ base: "none", md: "flex" }}
+      >
         <TextComponent>WHITELIST</TextComponent>
         <TextComponent>ABOUT</TextComponent>
         <TextComponent>MINTING</TextComponent>
         <TextComponent>ROADMAP</TextComponent>
         <TextComponent>MARKETPLACE</TextComponent>
-        <TextComponent color='#AD203E'>MERCH</TextComponent>
+        <TextComponent color="#AD203E">MERCH</TextComponent>
         <TextComponent>FAQ</TextComponent>
         <TextComponent>CREATE WALLET</TextComponent>
       </Flex>
-      <Image src={burgerIcon} w="19px" h='19px' display={{base:'block' , md:'none'}} />
+      <Image
+        src={burgerIcon}
+        w="19px"
+        h="19px"
+        display={{ base: "block", md: "none" }}
+      />
     </Flex>
   );
 };
@@ -34,9 +57,9 @@ const TextComponent = ({ children, color }) => {
     <Text
       fontFamily="Montserrat"
       fontWeight="700"
-      fontSize={{base:"4px" ,md:'10px' , xl:'15px' }}
+      fontSize={{ base: "4px", md: "10px", xl: "15px" }}
       color={color ? color : "#161F2F"}
-      cursor='pointer'
+      cursor="pointer"
     >
       {children}
     </Text>
